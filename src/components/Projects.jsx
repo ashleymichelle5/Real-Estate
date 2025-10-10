cae0dd4dea08dfa409041fc622bea186c10cdd9a
Ashley Michelle <ashleysanchez@MacBook-Pro.local>
2025-10-09 20:46:00 -0400

    projects and testimonials done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import left_arrow from '../assets/left_arrow.svg'
 import right_arrow from '../assets/right_arrow.svg'
 import { projectsData,assets } from '../assets/assets'
 
 const Projects = () => {
+
+const [currentIndex, setCurrentIndex] = React.useState(0);
+const [cardsToShow, setCardsToShow] = React.useState(1);
+
+useEffect(() => {
+    const updateCardsToShow = () => {
+        if(window.innerWidth >= 1024) {
+            setCardsToShow(projectsData.length);
+        } else {
+            setCardsToShow(1);
+        }
+    };
+        updateCardsToShow();
+
+        window.addEventListener('resize', updateCardsToShow);
+        return () => window.removeEventListener('resize', updateCardsToShow);
+
+},[])
+
+const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
+}
+const prevProject = () => {
+    setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1);
+}
+
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
@@ -11,17 +37,17 @@ const Projects = () => {
     
     {/* Slider buttons */}
     <div className='flex justify-end items-center mb-8'>
-        <button className='p-3 bg-gray-200 rounded mr-2 ' aria-label='Previous Project'>
+        <button onClick={prevProject} className='p-3 bg-gray-200 rounded mr-2 ' aria-label='Previous Project'>
             <img src={left_arrow} alt="Previous" />
         </button>
-         <button className='p-3 bg-gray-200 rounded mr-2 ' aria-label='Next Project'>
+         <button onClick={nextProject} className='p-3 bg-gray-200 rounded mr-2 ' aria-label='Next Project'>
             <img src={right_arrow} alt="Next" />
         </button>
     </div>
 
     {/* Project slider container */}
     <div className='overflow-hidden'>
-        <div className='flex gap-8 transition-transform duration-500 ease-in-out'>
+        <div className='flex gap-8 transition-transform duration-500 ease-in-out'style={{transform:`translateX(-${(currentIndex * 100) / cardsToShow}%)`}} >
             {projectsData.map((project,index)=> (
                 <div key={index} className='relative flex-shrink-0 w-full sm:w-1/4'>
                     <img src={project.image} alt={project.title} className='w-full h-auto mb-14' />
@@ -31,7 +57,7 @@ const Projects = () => {
                                 {project.title}
                             </h2>
                             <p className='text-gray-500 text-sm'>
-                                {project.price} <span></span> {project.location}
+                                {project.price} <span className='px-1'></span> {project.location}
                             </p>
                         </div>
                     </div>
