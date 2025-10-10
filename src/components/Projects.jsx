0b6871d6a28c517889a1a4f1c39a2cddcec90493
Ashley Michelle <ashleysanchez@MacBook-Pro.local>
2025-10-10 12:24:02 -0400

    components done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import left_arrow from '../assets/left_arrow.svg'
 import right_arrow from '../assets/right_arrow.svg'
 import { projectsData,assets } from '../assets/assets'
+import { motion } from 'framer-motion'
 
 const Projects = () => {
 
@@ -31,7 +32,12 @@ const prevProject = () => {
 }
 
   return (
-    <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
+    <motion.div 
+    initial={{opacity:0, x:-200}}
+    transition={{duration:1}}
+    whileInView={{opacity:1, x:0}}
+    viewport={{once:true}}
+    className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto '>Crafting Spaces, Buiding Legacies-Explore Our Portfolio</p>
     
@@ -65,7 +71,7 @@ const prevProject = () => {
             ))}
         </div>
     </div>
-    </div>
+    </motion.div>
   )
 }
 
