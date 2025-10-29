5c64018998cf9ed7152f2df7061870f43e6b0c58
Ashley Sanchez <ashleymichelle0511@gmail.com>
2025-10-28 22:38:26 -0400

    Update body overflow style for mobile menu
    Change body overflow style to 'auto' when mobile menu is shown.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     
 useEffect(() => {
     if(showMobileMenu) {
-        document.body.style.overflow = 'hidden';
+        document.body.style.overflow = 'auto';
     } else {
         document.body.style.overflow = 'auto';
     }
