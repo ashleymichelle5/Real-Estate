052ee36c21da307ca512f8b9914a2260b7e28003
Ashley Sanchez <ashleymichelle0511@gmail.com>
2025-10-29 12:45:58 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,9 +13,9 @@ const Header = () => {
       viewport={{once:true}}
       className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
         <h2 className='text-5xl sm:text-6xl md:text[82px] inline-block max-w-3xl font-semibold pt-20'>Real Estate that will move you.</h2>
-        <a href="#Contact" className='bg-blue-500 hover:bg-blue-600 px-4 sm:px-8 py-3 rounded-lg text-white font-semibold transition-all duration-300 text-sm sm:text-base block mx-auto text-center mt-8 w-fit'>
+        <a href="#Contact" className='bg-blue-500 hover:bg-blue-600 px-4 sm:px-8 py-4 rounded-lg text-white font-semibold transition-all duration-300 text-sm sm:text-base block mx-auto text-center mt-8 w-fit'>
           <span className='hidden sm:inline'>SCHEDULE A FREE HOME CONSULTATION</span>
-          <span className='sm:hidden mt-8'>Free Consultation</span>
+          <span className='sm:hidden text-lg font-bold mt-8'>FREE CONSULTATION</span>
         </a>
       </motion.div>
     </div>
